cf5b84fd19e4131d729cb21eb229d7b8f454ff9a
Peter Stuifzand <peter@stuifzand.eu>
2018-04-08 02:04:38 +0200

    Better formatted date

--- cmd/server/fetch.go
+++ cmd/server/fetch.go
@@ -156,7 +156,7 @@ func (b *memoryBackend) feedItems(fetchURL, contentType string, body io.Reader) 
 			item.Content.Text = feedItem.Content
 			item.URL = feedItem.Link
 			item.Id = hex.EncodeToString([]byte(feedItem.ID))
-			item.Published = feedItem.Date.Format(time.RFC822Z)
+			item.Published = feedItem.Date.Format(time.RFC3339)
 			log.Println(item)
 			log.Println(feedItem)
 			items = append(items, item)
