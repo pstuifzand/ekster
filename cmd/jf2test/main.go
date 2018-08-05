fddea5e39bb08157c709569db9f99c64e3f09263
Peter Stuifzand <peter@stuifzand.eu>
2018-08-05 13:57:42 +0200

    jf2test - add testing program for JF2 output

--- cmd/jf2test/main.go
+++ cmd/jf2test/main.go
@@ -1,1 +1,42 @@
-package jf2test
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"p83.nl/go/ekster/pkg/fetch"
+)
+
+func init() {
+	f, err := os.Open(os.DevNull)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(f)
+}
+
+type fetcher struct {}
+
+func (f fetcher) Fetch(url string) (*http.Response, error) {
+	return http.Get(url)
+}
+
+func main() {
+	cl := http.Client{}
+
+	url := os.Args[1]
+	resp, err := cl.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	items, err := fetch.FeedItems(nil, url, resp.Header.Get("Content-Type"), resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(os.Stdout).Encode(items)
+}
