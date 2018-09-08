bd05576f1c5042a3e72e7dd9a86218470c9200b9
Peter Stuifzand <peter@stuifzand.eu>
2018-09-08 19:32:01 +0200

    Add cache-control to allowed methods

--- cmd/eksterd/microsub.go
+++ cmd/eksterd/microsub.go
@@ -48,7 +48,7 @@ func (h *microsubHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodOptions {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
 		w.Header().Add("Access-Control-Allow-Methods", "GET, POST")
-		w.Header().Add("Access-Control-Allow-Headers", "Authorization")
+		w.Header().Add("Access-Control-Allow-Headers", "authorization, cache-control")
 		return
 	}
 
