90074d28d642e2cd633af2a98bcf8508d5733194
Peter Stuifzand <peter@stuifzand.eu>
2021-11-20 22:26:39 +0100

    Problem: licenses in files are not regular
    Solution: Paste license on top of all files. This does not change the
    license. It was already licensed as GPLv3.

--- pkg/jf2/simplify.go
+++ pkg/jf2/simplify.go
@@ -1,21 +1,22 @@
-// Package jf2 converts microformats to JF2
 /*
-   ekster - microsub server
-   Copyright (C) 2018  Peter Stuifzand
+ *  Ekster is a microsub server
+ *  Copyright (c) 2021 The Ekster authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Package jf2 converts microformats to JF2
 package jf2
 
 import (
--- pkg/util/algorithm.go
+++ pkg/util/algorithm.go
@@ -1,7 +1,26 @@
+/*
+ *  Ekster is a microsub server
+ *  Copyright (c) 2021 The Ekster authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package util
 
 import "reflect"
 
+// Rotate rotates values of an array, between index f and l with midpoint k
 func Rotate(a interface{}, f, k, l int) int {
 	swapper := reflect.Swapper(a)
 	if f == k {
@@ -38,6 +57,7 @@ func Rotate(a interface{}, f, k, l int) int {
 	return ret
 }
 
+// StablePartition partitions elements of the array between indices f and l according to predicate p
 func StablePartition(a interface{}, f, l int, p func(i int) bool) int {
 	n := l - f
 
@@ -48,7 +68,7 @@ func StablePartition(a interface{}, f, l int, p func(i int) bool) int {
 	if n == 1 {
 		t := f
 		if p(f) {
-			t += 1
+			t++
 		}
 		return t
 	}
